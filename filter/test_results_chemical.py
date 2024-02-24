094d6729399abf50c99531367d0387d3c4495880
Administroot <1474668090@qq.com>
2024-02-24 22:38:43 +0800

    :sparkles: 预处理过滤器框架

--- filter/test_results_chemical.py
+++ filter/test_results_chemical.py
@@ -1,5 +1,6 @@
 from models import scrutinizer, chemical
 from utils import df_to_list, general_preprocessing_filter
+from pandas import DataFrame
 import peewee as pw
 
 
@@ -15,8 +16,8 @@ def df_to_sql(db: pw.Database, srt: scrutinizer) -> None:
         db.drop_tables([chemical])
         db.create_tables([chemical])
 
-    # 调用通用预处理过滤器
-    data = general_preprocessing_filter(srt.data, [0, 1, 2])
+    # 调用过滤器（特定、通用）
+    data = preprocessing_filter(srt.data, [0, 1, 2])
     # srt.data.ffill(axis=0)
 
     # dataframe -> list
@@ -38,3 +39,22 @@ def df_to_sql(db: pw.Database, srt: scrutinizer) -> None:
     # 数据写入Sqlite3数据库
     chemical.insert_many(data, title).execute()
 
+
+########## 预处理过滤器 ###########
+def preprocessing_filter(df: DataFrame, column: list):
+    """预处理过滤器
+    :param df: 数据对象
+    :param column: 需要进行padding操作的列号 (FUNC utils.excel_row_padding)
+    :return: DataFrame
+    """
+    # 特定过滤器
+    data = check_contact_time(df)
+    # 通用过滤器
+    data = general_preprocessing_filter(data, column)
+    return data
+
+
+def check_contact_time(df: DataFrame) -> DataFrame:
+    # TODO: 每个操作点的接害时间应当一致，如果不一致弹出警告让用户确认是否进行下一步
+    pass
+    return df
